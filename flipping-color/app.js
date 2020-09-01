90cd55c263b2059a4371a0cb3421b6c05237019d
Ghevi <ghevi96@gmail.com>
2020-09-02 00:53:15 +0200

    Added fixed colors section

--- flipping-color/app.js
+++ flipping-color/app.js
@@ -1,1 +1,12 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['blue', 'orangered', 'green', 'yellowgreen', 'rgba(133,122,200)', '#f15025'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+const getRandomNumber = () => Math.floor(Math.random() * colors.length)
+
+btn.addEventListener('click', () => {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+})
